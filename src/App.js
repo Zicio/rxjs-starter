2b8c3fb2c567abc2aa64a27f05e2226a5e55cd4a
Alexander Burachkov <zicio@yandex.ru>
2023-03-20 21:08:45 +0400

    Solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import {ajax} from "rxjs/ajax";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [data, setData] = useState(null);
+  useEffect( () => {
+    setData(() => {ajax.getJSON('https://api.github.com/search/repositories?q=rxjs').subscribe((value) => console.log(value));})
+  }, [])
+  return <div>{data}</div>
 }
 
 export default App;
